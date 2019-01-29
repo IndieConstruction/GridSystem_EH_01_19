167798acef6238342564cacae84df693d6064d39
Brago <brago@indieconstruction.com>
2019-01-29 18:27:33 +0100

    creato grid system base

---
+++ Assets/GridSystem/Scripts/Cell.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace GridSystem {
+
+    public class Cell {
+
+        internal int x;
+        internal int y;
+        internal Vector3 worldPosition;
+
+        public Cell() {
+
+        }
+
+        public Cell(int _x, int _y, Vector3 _worldPosition) {
+            x = _x;
+            y = _y;
+            worldPosition = _worldPosition;
+        }
+
+    }
+
+}
---
+++ Assets/GridSystem/Scripts/PositionTester.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GridSystem;
+
+public class PositionTester : MonoBehaviour {
+
+    public int x;
+    public int y;
+
+    public BaseGrid grid;
+
+	// Update is called once per frame
+	void Update () {
+        if (grid) {
+            transform.position = grid.GetWorldPosition(x, y);
+        }
+        
+	}
+}
