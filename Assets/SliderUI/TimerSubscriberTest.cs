926f0632b06e72d9aa22615d69dfcb58c4382fa4
Paolo Bragonzi <brago@indieconstruction.com>
2019-03-05 18:34:39 +0100

    Timer controller con slider e delegate

---
+++ Assets/SliderUI/TimerSubscriberTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerSubscriberTest : MonoBehaviour
+{
+    public Slider slider;
+
+    private void OnEnable() {
+        slider.EndTimer += endTimerDelegate;
+    }
+
+    private void endTimerDelegate() {
+        Debug.Log("Timer terminato");
+    }
+
+    private void OnDisable() {
+        slider.EndTimer -= endTimerDelegate;
+    }
+
+}
