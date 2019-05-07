79a6f78380b040d11d7541373951fd793f8eddae
Paolo Bragonzi <brago@indieconstruction.com>
2019-05-07 18:22:03 +0200

    StateMachine v.1.2 (con animator)

---
+++ Assets/StateMachineAnim/FlowSM/States/Setup_State.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Setup_State : StateBehaviourBase {
+
+
+    public override void OnEnter() {
+        
+    }
+
+    public override void OnExit() {
+        ctx.SetupDone = true;
+        Debug.LogFormat("SetupState {0} in Setup_State", ctx.SetupDone);
+    }
+
+}
