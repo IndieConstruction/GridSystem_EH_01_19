fb7912afad1f90c76c4f99b7796910d6f8ad64e4
Paolo Bragonzi <brago@indieconstruction.com>
2019-06-25 16:14:14 +0200

    Aggiunto simple audio manager

---
+++ Assets/Audio/SympleAudioManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SympleAudioManager : MonoBehaviour {
+
+    [Header("Audio Sources")]
+    public List<AudioSource> FXSources;
+    private int fxSourcesIndex;
+    public AudioSource MusicSources;
+    
+
+    [Header("Audio Clips Music")]
+    public AudioClip MusicMenuClip;
+    public AudioClip MusicGameplayClip;
+
+    [Header("Audio Clips FX")]
+    public AudioClip FXClip1;
+
+    public void PlayMenuMusic() {
+        MusicSources.clip = MusicMenuClip;
+        MusicSources.Play();
+    }
+
+    public void PlayGameplayMusic() {
+        MusicSources.clip = MusicGameplayClip;
+        MusicSources.Play();
+    }
+
+    public void PlayFX1() {
+        FXSources[fxSourcesIndex].clip = FXClip1;
+        FXSources[fxSourcesIndex].Play();
+        fxSourcesIndex++;
+        if (fxSourcesIndex >= FXSources.Count)
+            fxSourcesIndex = 0;
+    }
+}
